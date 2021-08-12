59eac55cf777d91d28ede79b6c73fddb5d8c8813
IgorRoc <igor_roc@hotmail.com.br>
2021-08-12 12:38:58 -0300

    [new]: Nav scroll button

---
+++ js/nav.js
@@ -0,0 +1,53 @@
+var leftBtn = document.getElementsByClassName("leftNav")
+var rightBtn = document.getElementsByClassName("rightNav")
+
+for (const el of rightBtn) {
+	el.addEventListener("click", () => {
+		el.parentElement.scrollLeft += 800
+	})
+}
+
+for (const el of leftBtn) {
+	el.addEventListener("click", () => {
+		el.parentElement.scrollLeft -= 800
+	})
+}
+
+var wrappers = document.querySelectorAll(".wrapperCards")
+
+wrappers.forEach((el) => {
+    changeRandomCards(el)
+})
+
+// changeRandomCards(myList.children)
+
+function changeRandomCards(wrapper) {
+	var cards = ["babyDriver.webp", "desencanto.jpg", "disjointed.jpg", "finalSpace.jpg", "mundoMisterio.jpg"]
+    var vertical = ["lupin.webp", "strangerThings.webp"]
+    var min = 0
+    var maxC = cards.length
+    var maxV = vertical.length
+
+    var isVertical = false
+    console.log(wrapper.parentElement)
+    if(wrapper.parentElement.classList.contains("verticalCards")){
+        isVertical = true
+    }
+    if(wrapper.parentElement.classList.contains("loadingCards")){
+        return
+    }
+
+	for (const card of wrapper.children) {
+		if (
+			card.classList.contains("leftNav") ||
+			card.classList.contains("rightNav")
+		) {
+			continue
+		}
+        if(!isVertical){
+            card.children[0].children[0].src = `./assets/card_${cards[Math.floor(Math.random() * (maxC - min) + min)]}`
+        }else{
+            card.children[0].children[0].src = `./assets/vertical_${vertical[Math.floor(Math.random() * (maxV - min) + min)]}`
+        }
+	}
+}
